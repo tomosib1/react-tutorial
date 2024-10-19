5c3eb5c7a1a4ec332800bdca1f158e9ef424c09c
tomosibi <>
2024-10-19 17:16:43 +0900

    コンポーネントを純粋に保つ 完了

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import { people } from './data.js'
-import { getImageUrl } from './utils.js'
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>
+}
 
-export default function List() {
-  const chemists = people.filter((person) => person.profession === 'chemist')
-  const chemistListItems = chemists.map((person) => (
-    <li key={person.id}>
-      <img src={getImageUrl(person)} alt={person.name} />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  ))
-  const others = people.filter((person) => person.profession !== 'chemist')
-  const otherItemList = others.map((person) => (
-    <li key={person.id}>
-      <img src={getImageUrl(person)} alt={person.name} />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  ))
-  return (
-    <article>
-      <h1>Chemists</h1>
-      <ul>{chemistListItems}</ul>
-      <h1>Others</h1>
-      <ul>{otherItemList}</ul>
-    </article>
-  )
+export default function TeaGathering() {
+  let cups = []
+  for (let i = 1; i <= 12; i++) {
+    cups.push(<Cup key={i} guest={i} />)
+  }
+  return cups
 }
