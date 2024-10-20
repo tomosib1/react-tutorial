00acc5e021ca624eff96552778bee0a2debe3ab8
tomosibi <>
2024-10-20 23:52:29 +0900

    一連の state の更新をキューに入れる 完了

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import { useState } from 'react'
 
-export default function Form() {
-  const [to, setTo] = useState('Alice')
-  const [message, setMessage] = useState('Hello')
-
-  function handleSubmit(e) {
-    e.preventDefault()
-    setTimeout(() => {
-      alert(`You said ${message} to ${to}`)
-    }, 5000)
-  }
+export default function Counter() {
+  const [number, setNumber] = useState(0)
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        To:{' '}
-        <select value={to} onChange={(e) => setTo(e.target.value)}>
-          <option value="Alice">Alice</option>
-          <option value="Bob">Bob</option>
-        </select>
-      </label>
-      <textarea
-        placeholder="Message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button type="submit">Send</button>
-    </form>
+    <>
+      <h1>{number}</h1>
+      <button
+        onClick={() => {
+          setNumber(number + 5)
+          setNumber((n) => n + 1)
+          setNumber(42)
+        }}
+      >
+        Increase the number
+      </button>
+    </>
   )
 }
