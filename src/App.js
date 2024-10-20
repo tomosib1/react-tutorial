daa520a15f93d04ed3619f373c9812fc0ffff0d2
tomosibi <>
2024-10-20 23:22:31 +0900

    state はスナップショットである 完了

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-export default function Clock({ time }) {
+import { useState } from 'react'
+
+export default function Form() {
+  const [to, setTo] = useState('Alice')
+  const [message, setMessage] = useState('Hello')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTimeout(() => {
+      alert(`You said ${message} to ${to}`)
+    }, 5000)
+  }
+
   return (
-    <>
-      <h1>{time}</h1>
-      <input />
-    </>
+    <form onSubmit={handleSubmit}>
+      <label>
+        To:{' '}
+        <select value={to} onChange={(e) => setTo(e.target.value)}>
+          <option value="Alice">Alice</option>
+          <option value="Bob">Bob</option>
+        </select>
+      </label>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
   )
 }
